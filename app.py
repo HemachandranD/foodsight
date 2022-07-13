7674a677e68fe38d7e38cc456508b48aeb3c02b7
HemachandranD <hema18deena@gmail.com>
2022-07-13 17:26:53 +0530

    updated app icon

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 
 st.set_page_config(
     page_title="Food Sight",
-    page_icon=":pizza:eyes",
+    page_icon=":pizza,:eyes",
     initial_sidebar_state="expanded",
     menu_items={"About": "# This is an *extremely* cool Food Sight app!"},
 )
