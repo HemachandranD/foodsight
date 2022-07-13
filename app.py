e2f893cd8b82bd09a59144cad65228507a9f8f48
HemachandranD <hema18deena@gmail.com>
2022-07-13 15:46:30 +0530

    updated app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ file = st.file_uploader(
 )
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
-file = st.camera_input("Take a picture")
+img_file_buffer = st.camera_input("Take a picture")
 
 
 def upload_predict(upload_image, model, img_shape=224):
@@ -49,8 +49,16 @@ def upload_predict(upload_image, model, img_shape=224):
 
 if file is None:
     st.text("Please upload an image file")
-else:
+elif file is not None:
     image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions, pred_prob = upload_predict(image, model)
+    image_class = str(predictions)
+    score = f"{pred_prob.max():.2f}"
+    st.write("The is", image_class)
+    st.write("The Confidence score is approximately", score)
+elif img_file_buffer is not None:
+    image = Image.open(img_file_buffer)
     st.image(image, use_column_width=True)
     predictions, pred_prob = upload_predict(image, model)
     image_class = str(predictions)
